d3fa37c59fa8d65ecdefc4029b3d59410d4949ea
Yeamin <yeaminb1903@gmail.com>
2025-04-27 14:12:08 +0600

    initial commit

---
+++ Multiple Assignments.py
@@ -0,0 +1,18 @@
+Name = "Yeamin"
+Age = 25
+Institution = "Dhaka University"
+Experience = " Available on request"
+print(Name,Age,Institution,Experience) 
+
+a=10
+b=10
+c=10
+print(a,b,c)
+
+a=b=c=10
+print(a,b,c)
+
+a,b,c= 1,2,"Zara Ali"
+print(a)
+print(b)
+print(c)
