b426c87ce6e496424562f67f2539de5bcbe24b79
Yeamin <yeaminb1903@gmail.com>
2025-05-01 00:43:11 +0600

    initial commit

---
+++ string formating.py
@@ -0,0 +1,10 @@
+num1 = 50
+num2 = 20
+print("sum of the numbers = ", num1+num2)
+num1 = 76
+num2 = 24
+print(f"sum of the numbers = {num1+num2}")
+
+username = "Hablu"
+Roll = 10
+print(f"My name is {username} and my class roll is {Roll}")
