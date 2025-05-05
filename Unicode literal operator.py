1a62a0bb852a0d03a21bb391dd2c5ac345520dbc
Yeamin <yeaminb1903@gmail.com>
2025-05-05 10:48:11 +0600

    initial commit

---
+++ Unicode literal operator.py
@@ -0,0 +1,17 @@
+x = 10 #literal
+Y = x**2 #not literal
+
+a = 10
+b = 20
+print("addition of two integer")
+print("a=",a,' ' "b=",b,' ' "addition=",a+b)
+
+c = 10
+d = 20.5
+print("additon of integer and float")
+print("c=",c,' ' "d=",d,' ' "addition=",c+d)
+
+e = 25
+f = 10j
+print("additon of integer and complex")
+print("e=",e,' ' "f=",f,' ' "addition=",e+f)
---
+++ modulus operator.py
@@ -0,0 +1,7 @@
+a = 10
+b = 2
+print("Modulus= ",a%b)
+
+c = 10.5
+d = 2.5
+print("Modulus= ",c%d)
