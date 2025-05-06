4fa2b9f7e7e55375c06c51adc03212a1bccc435e
Yeamin <yeaminb1903@gmail.com>
2025-05-07 02:41:06 +0600

    initial commit

---
+++ Assignment gpa calculation.py
@@ -0,0 +1,19 @@
+Assignments = float(input("Enter your assignment marks:"))
+Tests = float(input("Enter your test score:"))
+LabWorks = float(input("Enter your lab score:"))
+
+finalscore = (Assignments * 0.10) + (Tests * 0.70) + (LabWorks * 0.20)
+
+if finalscore >=90:
+    grade = "A"
+elif finalscore >=80:
+    grade = "B"
+elif finalscore >=70:
+    grade = "C"
+elif finalscore >=60:
+    grade = "D"
+else:
+    grade = "Failed"
+
+
+print("Your final result/grade is:",grade)
