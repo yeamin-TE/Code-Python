c4e6d9cb7dc3599cb613780b37b6aa1381205047
Yeamin <yeaminb1903@gmail.com>
2025-05-07 00:35:24 +0600

    initial commit

---
+++ User input.py
@@ -0,0 +1,4 @@
+name= input("Enter your name:")
+city= input("Enter your city:")
+print("My name is",name)
+print("I am from",city)
---
+++ comparision operator.py
@@ -0,0 +1,7 @@
+a= 5
+b= 7
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print("a=",a, ' ' "b=",b, "a!=b", a!=b)
