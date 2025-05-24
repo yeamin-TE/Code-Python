1cc3e902d7d53b77ec99d04093f1bc5994f27e88
Yeamin <yeaminb1903@gmail.com>
2025-05-24 20:30:54 +0600

    initial commit

---
+++ function 1.py
@@ -0,0 +1,20 @@
+def say_hello():
+    print("Hello, I am learning python")
+
+say_hello()
+
+
+def add_numbers():
+    a=4
+    b=7
+    result= a+b 
+    print("Sum=",result)
+
+add_numbers()
+
+
+def greet(name):
+    print("How are you?",name)
+
+greet("Yeamin")
+greet("Meher")
---
+++ function 2.py
@@ -0,0 +1,43 @@
+def add(a,b):
+    result= a+b
+    print("Sum=",result)
+
+add(4,5)
+add(5,89)
+
+def subtract(c,d):
+    result= c-d 
+    print("Subtraction=",result)
+
+subtract(5,4)
+subtract(26584,15458)
+subtract(25454,7843452)
+
+def multiply(e,f):
+    result= e*f
+    print("Product=",result)
+
+multiply(5,10)
+multiply(45,2)
+
+def divide(a,b):
+    result= a/b
+    print("Quotient=",result)
+
+divide(500,25)
+divide(25,5)
+
+def percentage(a,b):
+    result=(a/b)*100
+    print("percentage is:",result,"%")
+
+percentage(80,250)
+percentage(25,150)
+
+def average(a,b,c):
+    result=(a+b+c)/3
+    print("Average=",result)
+
+average(10,15,58)
+average(5,10,20)
+
