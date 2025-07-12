b25483884dedcc3702798c3e6c131cd897d1ee95
Yeamin <yeaminb1903@gmail.com>
2025-07-12 14:11:42 +0600

    initial commit

---
+++ python class 11(1).py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+arr = np.array([1,2,3,4])
+print("1D array", arr)
+
+arr2 = np.array([[1,2],[3,4]])
+print("2d array \n",arr2)
+
+arr3 = np.array([[1,2],[3,4],[5,6]])
+print("3d array \n", arr3)
