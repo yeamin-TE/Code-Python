88f81391e1df7bbcb2aa10b212537b217b3462e1
Yeamin <yeaminb1903@gmail.com>
2025-07-15 16:37:07 +0600

    first commit

---
+++ Python Class 11 (2).py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+sample_array = np.zeros((3,4))
+print(sample_array)
+print("shape:", sample_array.shape)
+print("size:", sample_array.size)
+print("data type", sample_array.dtype)
---
+++ Python Class 11 (3).py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+arr = np.array([1,2,3,4])
+print(arr)
+print("add 1", arr+1)
+print("multiply", arr * 2)
+print("sum", arr.sum())
+print("mean", arr.mean())
+
+zeros= np.zeros((2,3))
+print(zeros)
+
+ones = np.ones((2,3))
+print(ones)
+
+ran= np.random.rand(2,3)
+print(ran)
+
+arr2 = np.array([10,15,20,25,30,35])
+print(arr2)
+print("sum", arr2.sum())
+print("mean", arr2.mean())
+print("standard deviation", arr2.std())
+print("max", arr2.max())
+print("Index of max", arr2.argmax())
+print("min", arr2.min())
+print("Index of min", arr2.argmin())
+
+#Matrix Application: 
+a = np.array([[2,3],
+              [5,6]])
+b = np.array([[10,12],
+              [13,15]])
+print("Matrix multiplication", a@b)
+print("element multiplication", a*b)
